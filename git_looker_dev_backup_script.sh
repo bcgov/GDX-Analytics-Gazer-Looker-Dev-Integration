fd3d9b80e7d4c73ac62e3a15dc1d3ad2496c5a8f
Veenu Punyani <veenu.veenu@gov.bc.ca>
2023-09-27 08:59:58 +0000

    Backup Looker data at 2023-09-27 08:59:58

--- git_looker_dev_backup_script.sh
+++ git_looker_dev_backup_script.sh
@@ -4,7 +4,7 @@ cd /home/looker/GDX-Analytics-Gazer-Looker-Dev-Integration
 # Run the gzr space export commands
 gzr space export 21 --no-verify-ssl --host=dev.analytics.gov.bc.ca
 gzr space export 11 --no-verify-ssl --host=dev.analytics.gov.bc.ca
-gzr space export 31 --no-verify-ssl --host=dev.analytics.gov.bc.ca
+gzr space export 32 --no-verify-ssl --host=dev.analytics.gov.bc.ca
 
 
 # Commit all changes with a customized message
